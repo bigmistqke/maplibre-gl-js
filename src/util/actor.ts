9fea4a3c9f76561fcbcf8c1e52589a7e3edc47e7
vincent van dijck <vandijckv@gmail.com>
2025-08-28 13:14:02 +0200

    fix: replace PostMessageOptions with Tranferable[] see https://gitlab.com/radiogarden/mono/-/issues/2831

--- src/util/actor.ts
+++ src/util/actor.ts
@@ -135,7 +135,9 @@ export class Actor implements IActor {
                 origin: location.origin,
                 data: serialize(message.data, buffers)
             };
-            this.target.postMessage(messageToPost, {transfer: buffers});
+            this.target.postMessage(messageToPost,
+                // @ts-expect-error
+                buffers);
         });
     }
 
@@ -241,7 +243,9 @@ export class Actor implements IActor {
             error: err ? serialize(err) : null,
             data: serialize(data, buffers)
         };
-        this.target.postMessage(responseMessage, {transfer: buffers});
+        this.target.postMessage(responseMessage,
+            // @ts-expect-error
+            buffers);
     }
 
     remove() {
