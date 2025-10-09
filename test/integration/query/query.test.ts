ff78b747e1d6d0fe3ad92afd5c03b2fe9da20182
vincent van dijck <vandijckv@gmail.com>
2025-10-09 17:42:45 +0200

    fix: fix type imports in tests

--- test/integration/query/query.test.ts
+++ test/integration/query/query.test.ts
@@ -12,15 +12,15 @@ import type {AddressInfo} from 'node:net';
 import {deepEqual} from '../lib/json-diff';
 import {localizeURLs} from '../lib/localize-urls';
 import {launchPuppeteer} from '../lib/puppeteer_config';
-import type {default as MapLibreGL} from '../../../dist/maplibre-gl';
+import type * as MapLibreGL from '../../../src';
 
 let maplibregl: typeof MapLibreGL;
 
 async function performQueryOnFixture(fixture)  {
 
-    async function handleOperation(map: maplibregl.Map, operation) {
+    async function handleOperation(map: MapLibreGL.Map, operation) {
         const opName = operation[0];
-        
+
         switch (opName) {
             case 'wait':
                 while (!map.loaded()) {
@@ -114,7 +114,7 @@ describe('query tests', () => {
             <html lang="en">
             <head>
                 <meta charset='utf-8'>
-                
+
             </head>
             <body id='map'></body>
             </html>`);
@@ -138,7 +138,7 @@ describe('query tests', () => {
             }
             return entry;
         });
-        
+
         const coverageReport = new CoverageReport({
             name: 'MapLibre Coverage Report',
             outputDir: './coverage/query',
@@ -148,9 +148,9 @@ describe('query tests', () => {
             }
         });
         coverageReport.cleanCache();
-        
+
         await coverageReport.add(rawV8CoverageData);
-        
+
         await coverageReport.generate();
     }, 60000);
 
